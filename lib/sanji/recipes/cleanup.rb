3392a577726bf717eae4ddaffe125841292d1e2d
kbparagua <kb.paragua@gmail.com>
2015-12-17 15:51:14 +0800

    Gemfile Cleanup recipe

--- lib/sanji/recipes/cleanup.rb
+++ lib/sanji/recipes/cleanup.rb
@@ -6,63 +6,6 @@ class Sanji::Recipes::Cleanup < Sanji::Recipe
       'config/application.rb',
       'Gemfile'
     ].each { |file| a.gsub_file file, /^\s*# sanji.*\n/, '' }
-
-    self.remove_gemfile_comments
-
-    gemfile = File.read "#{a.destination_root}/Gemfile"
-
-    gem_groups = {:root => []}
-    active_group = :root
-    gemfile.each_line do |line|
-      if line == self.group_header(:development, :test)
-        active_group = ':development, :test'
-        next
-      elsif line == self.group_header(:development)
-        active_group = ':development'
-        next
-      elsif line == self.group_header(:test)
-        active_group = ':test'
-        next
-      end
-
-      if line == "end\n"
-        active_group = :root
-        next
-      end
-
-      gem_groups[active_group] ||= []
-      gem_groups[active_group].push line
-    end
-
-    File.open("#{a.destination_root}/Gemfile", 'w'){ |f| f.truncate(0) }
-
-    non_group_statements = gem_groups.delete :root
-    non_group_statements.each do |line|
-      line = "#{line}\n" unless line.include?("\n")
-      a.append_to_file 'Gemfile', line
-    end
-
-    gem_groups.each do |group, lines|
-      a.append_to_file 'Gemfile', "group #{group} do\n"
-
-      lines.each do |line|
-        line = "#{line}\n" unless line.include?("\n")
-        a.append_to_file 'Gemfile', line
-      end
-
-      a.append_to_file 'Gemfile', "end\n"
-    end
-  end
-
-  protected
-
-  def remove_gemfile_comments
-    a.gsub_file 'Gemfile', /^\s*#.*\n/, ''
-  end
-
-  def group_header *groups
-    group_string = groups.map { |g| ":#{g}" }.join ', '
-    "group #{group_string} do\n"
   end
 
 end
---
+++ lib/sanji/recipes/gemfile_cleanup.rb
@@ -0,0 +1,77 @@
+require 'pp'
+
+class Sanji::Recipes::GemfileCleanup < Sanji::Recipe
+
+  def after_everything
+    @gemfile = File.read self.gemfile_path
+    @new_gemfile = ''
+
+    @group_lines = {:global => []}
+    @active_group = :global
+
+    @gemfile.each_line { |line| self.group_line line }
+
+    self.rewrite_gemfile
+  end
+
+
+
+  protected
+
+  def gemfile_path
+    @gemfile_path ||= "#{a.destination_root}/Gemfile"
+  end
+
+  def group_line line = ''
+    # Ignore comments and empty lines
+    return if line =~ /^\s*#/ || line =~ /^\s*$\n/
+
+    group_full_name = self.extract_group_full_name line
+
+    if group_full_name.present?
+      @active_group = group_full_name
+    elsif line =~ /^end$\n?/
+      @active_group = :global
+    else
+      @group_lines[@active_group] ||= []
+      @group_lines[@active_group].push line
+    end
+  end
+
+  def extract_group_full_name line = ''
+    if line =~ /^group.+do$\n/
+      groups = line.scan(/:\w+/).sort
+      full_name = groups.join ', '
+
+      return full_name
+    end
+
+    nil
+  end
+
+  def rewrite_gemfile
+    self.erase_gemfile_contents
+
+    global_statements = @group_lines.delete :global
+    global_statements.each { |line| @new_gemfile << line }
+
+    @group_lines.each do |group, lines|
+      self.append_group_to_new_gemfile group, lines
+    end
+
+    a.append_to_file 'Gemfile', @new_gemfile
+  end
+
+  def erase_gemfile_contents
+    File.open(self.gemfile_path, 'w'){}
+  end
+
+  def append_group_to_new_gemfile group, lines
+    @new_gemfile << "\ngroup #{group} do\n"
+
+    lines.each { |line| @new_gemfile << line }
+
+    @new_gemfile << "end\n"
+  end
+
+end
