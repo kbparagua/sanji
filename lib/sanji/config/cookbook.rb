39133e019e297f9e86759f8564503ce5c13fee6d
kbparagua <kb.paragua@gmail.com>
2016-01-09 13:08:09 +0800

    Logic for creating recipes inside Config::Cookbook

--- lib/sanji/config/cookbook.rb
+++ lib/sanji/config/cookbook.rb
@@ -1,20 +1,48 @@
 class Sanji::Config::Cookbook
 
+  COOKBOOK_REFERENCE_PREFIX = '^'
+
+  attr_reader :name
+
+
   def initialize name, contents = {}
     @name = name
     @contents = contents
-    @included_cookbooks = []
+    @included_cookbooks = {}
   end
 
   def include_cookbook cookbook
-    @included_cookbooks.push cookbook
+    @included_cookbooks[cookbook.name] = cookbook
   end
 
   def recipes
     @recipes ||=
-      @contents['recipes'].map do |name|
-        Sanji::Config::Recipe.new name
+      @contents['recipes'].flat_map do |value|
+        self.create_recipe value
       end
+  end
+
+
+
+  protected
+
+  def create_recipe recipe_or_reference
+    if recipe_or_reference.start_with? COOKBOOK_REFERENCE_PREFIX
+      self.create_recipes_from_cookbook recipe_or_reference
+    else
+      Sanji::Config::Recipe.new recipe_or_reference
+    end
+  end
+
+  def create_recipes_from_cookbook reference
+    name = reference.sub COOKBOOK_REFERENCE_PREFIX, ''
+
+    unless @included_cookbooks.has_key? name
+      raise "You failed to include cookbook: #{name}."
+    end
+
+    cookbook = @included_cookbooks[name]
+    cookbook.recipes
   end
 
 end
