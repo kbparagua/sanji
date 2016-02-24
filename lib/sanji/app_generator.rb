55768199dbf6d1556ef92c5f2f5f7538964c2aee
kbparagua <kb.paragua@gmail.com>
2016-02-24 17:56:58 +0800

    Remove puts

--- lib/sanji/app_generator.rb
+++ lib/sanji/app_generator.rb
@@ -5,8 +5,6 @@ module Sanji
   class AppGenerator < Rails::Generators::AppGenerator
 
     def finish_template
-      puts "source_paths = #{self.source_paths}"
-
       invoke :sanji_after_create_tasks
       super
     end
