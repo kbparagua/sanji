66bc3a8d04f9ade5501ce787f1a012f332c74699
kbparagua <kb.paragua@gmail.com>
2016-02-26 23:58:59 +0800

    Refactor Config::Cookbook

--- lib/sanji/config/cookbook.rb
+++ lib/sanji/config/cookbook.rb
@@ -1,77 +1,82 @@
-class Sanji::Config::Cookbook
+module Sanji::Config
+  class Cookbook
 
-  COOKBOOK_REFERENCE_PREFIX = '^'
-
-  attr_reader :name
+    attr_reader :name
 
 
-  def initialize name, contents = {}
-    @name = name
-    @contents = contents
-    @included_cookbooks = []
-  end
+    def initialize name, contents = {}
+      @name = name
+      @contents = contents
+      @included_cookbooks = []
+    end
 
-  def include_cookbook cookbook
-    @included_cookbooks << cookbook
-  end
+    def include_cookbook cookbook
+      @included_cookbooks << cookbook
+    end
 
-  def recipes
-    return @recipes if @recipes
+    def recipes
+      @recipes ||= self.included_recipes + self.own_recipes
+    end
 
-    @recipes = @included_cookbooks.flat_map &:recipes
+    def optional_recipes
+      @optional_recipes ||=
+        self.included_optional_recipes + self.own_optional_recipes
+    end
 
-    @recipes +=
-      @contents['recipes'].flat_map do |recipe|
-        Sanji::Config::Recipe.new recipe
+    def gem_groups
+      @gem_groups ||=
+        self.own_gem_groups.merge(self.included_gem_groups) do |key, own, other|
+          own + other
+        end
+    end
+
+
+
+    protected
+
+    def own_recipes
+      @contents['recipes'].map { |recipe| Recipe.new recipe }
+    end
+
+    def own_optional_recipes
+      result = @contents['optional'] || []
+      result.map { |name| Recipe.new name }
+    end
+
+    def own_gem_groups
+      gems_by_group_name = @contents['gems'] || {}
+
+      gem_groups = {}
+
+      gems_by_group_name.each do |group_name, gem_full_names|
+        group = group_name.split(' ').map &:strip
+        gem_groups[group] ||= []
+        gem_groups[group] += gem_full_names.map { |fn| Gem.new fn }
       end
 
-    @recipes
-  end
-
-  def optional_recipes
-    return @optional_recipes if @optional_recipes
-
-    optional = @contents['optional'] || []
-    optional.map! { |name| Sanji::Config::Recipe.new name }
-
-    @included_cookbooks.each do |cookbook|
-      optional += cookbook.optional_recipes
+      gem_groups
     end
 
-    @optional_recipes = optional
-  end
+    def included_recipes
+      @included_cookbooks.flat_map &:recipes
+    end
 
-  def gem_groups
-    return @gem_groups if @gem_groups
+    def included_optional_recipes
+      @included_cookbooks.flat_map &:optional_recipes
+    end
 
-    @gem_groups = self.own_gem_groups
+    def included_gem_groups
+      gems_by_group = {}
 
-    @included_cookbooks.each do |cookbook|
-      cookbook.gem_groups.each do |group, gems|
-        @gem_groups[group] ||= []
-        @gem_groups[group] += gems
+      @included_cookbooks.each do |cookbook|
+        cookbook.gem_groups.each do |group, gems|
+          gems_by_group[group] ||= []
+          gems_by_group[group] += gems
+        end
       end
+
+      gems_by_group
     end
 
-    @gem_groups
   end
-
-
-
-  protected
-
-  def own_gem_groups
-    gems_by_group_name = @contents['gems'] || {}
-
-    gem_groups = {}
-
-    gems_by_group_name.each do |group_name, gem_full_names|
-      group = group_name.split(' ').map &:strip
-      gem_groups[group] ||= []
-      gem_groups[group] += gem_full_names.map { |fn| Sanji::Config::Gem.new fn }
-    end
-
-    gem_groups
-  end
-
 end
